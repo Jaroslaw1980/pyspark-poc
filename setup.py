b6ad07d6f93a003e8ed94477a41647ac4166ac6c
Jaroslaw Nazar <jaroslawnazar@gmail.com>
2024-01-16 14:09:04 +0100

    setup file change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     author='Jarosław Nazar',
     author_email='jaroslaw.nazar@capgemini.com',
-    url='',
+    url='https://github.com/Jaroslaw1980/pyspark-poc.git',
     description='PySpark poc with loading, filtering, dropping joining and saving 2 csv files',
     license='MIT licence',
     packages=find_packages(exclude=["test"]),
